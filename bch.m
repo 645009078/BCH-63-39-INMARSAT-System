c6ba79b152bca8954808106c80a5a076dd3db330
EricNRodriguez <rodriguez.eric.n@gmail.com>
2020-04-14 22:31:29 +1000

    basic comments, decoding not implemented

--- bch.m
+++ bch.m
@@ -40,36 +40,54 @@ classdef bch
              [~, rem] = gfdeconv(msg_shifted, obj.gen_poly); % get remainder
              code_polynomial = gfadd(msg_shifted, rem, 2); 
         end
-        function ps_inds = gf_power_set_indexes(obj, i, max_pow) % i from alpha^i
+        function ps_exponents = gf_power_set_indexes(obj, i, max_pow)
             i = mod(i, 2^obj.m-1);
-            ps_inds = zeros(size(i, 2), max_pow+1);
-            ps_inds(:, 1) = 2; % 1
-            ps_inds(:, 2) = i' + 2;
+            ps_exponents = zeros(size(i, 2), max_pow+1);
+            % calculate power set exponents 
+            ps_exponents(:, 1) = 2; 
+            ps_exponents(:, 2) = i' + 2;
             pows = 2:max_pow;
-            ps_inds(:, pows+1) = mod(i' .* pows, 2^obj.m-1) + 2;
+            ps_exponents(:, pows+1) = mod(i' .* pows, 2^obj.m-1) + 2;
         end 
         function res = gf_poly_eval(obj, poly_coeffs, i) % i is for alpha^i
             i = mod(i, 2^obj.m-1);
             
+            % calculate row indexes for each term of each power set 
             power_set_indexes = obj.gf_power_set_indexes(i, size(poly_coeffs,2)-1); 
-            % convert to row major order
+            % convert to row major order for grouping (reshaping) 
             power_set_indexes = power_set_indexes';
             power_set_indexes = power_set_indexes(:)';
             
-            prod = obj.gf_ext.Polynomial(power_set_indexes, :) .* repmat(poly_coeffs, 1, size(i, 2))';
-            power_set_polynomials = reshape(prod', [obj.m, size(poly_coeffs, 2), size(i, 2)]);
+            % calculate column vector [(alpha^i)^1;...; (alpha^i)^p], for
+            % all i. p is the degree of the input polynomial.
+            power_set_polynomials = obj.gf_ext.Polynomial(power_set_indexes, :);
+            % repeat for each power set
+            repeated_coeffs = repmat(poly_coeffs, 1, size(i, 2));
+            % element wise multiplication with respective coefficients 
+            power_set_polynomials = power_set_polynomials .* repeated_coeffs';
+            
+            % reshape power set, grouping polynomial form of power set of 
+            % alpha^i in power_set_polynomials(:,:,i)
+            power_set_polynomials = reshape(power_set_polynomials', [obj.m, size(poly_coeffs, 2), size(i, 2)]);
+            
+            % eval the polynomial, adding the terms of each set multiplied
+            % by their coefficients. summing c0*1 + c1*alpha^i + ... +
+            % cp*(alpha^i)^p for each i
             res = mod(sum(power_set_polynomials, 2), 2);
             res = reshape(res, [obj.m,size(i,2),1])';
             res = res(:,:,1)
 
         end 
         function s_indexs = syndrome_indexes(obj, poly) % fix this to use matrix mult with parity check matrix in the future, for now its fine 
-            s_indexs = zeros(1, 2*obj.t); % col i is row of si in gf_ext
+            % elements [alpha, ...., alpha^2t] of gf(2^m)
+            s_indexs = zeros(1, 2*obj.t); 
             i =  1:2*obj.t;
+            % evaluate polynomial for all elements 
             si_poly = obj.gf_poly_eval(poly,i);
+            % convert result for each input alpha^i to decimal format.
             si_dec = bi2de(si_poly);
-            
             for n = i
+                % find row of result in gf_ext table
                 [s_indexs(n), ~] = find(obj.gf_ext.Decimal == si_dec(n), 1);
             end 
             return;
