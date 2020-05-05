37b8bc4e3146d9228b948b5f7a58e4956371f629
EricNRodriguez <rodriguez.eric.n@gmail.com>
2020-05-05 13:40:04 +1000

    save before fixing syndrome calc to not use indexes

--- scripts/main.m
+++ scripts/main.m
@@ -1,5 +1,5 @@
-clear
-clc
+% clear
+% clc
 
 % add files in src  
 addpath('../src/');
@@ -9,14 +9,13 @@ prim_poly = [1,1,0,0,0,0,1];
 code = bch(63,39,9,4,gen_poly,prim_poly );
 
 msg = zeros(1, 39);
-msg(1:2:39) = 1;
+% msg(1:2:39) = 1;
 
 code_poly = code.encode(msg);
 
 code_poly_altered = code_poly;
-code_poly_altered(38) = 1;
-code_poly_altered(1:2) = 0;
-code_poly_altered(12) = 1;
+code_poly
+code_poly_altered(19) = 1;
 
 r_msg = code.decode(code_poly_altered);
 
